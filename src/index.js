b7240a5b154585f914caeffc61d67763f09d495c
Andrey131 <36641490+Andrey131@users.noreply.github.com>
2018-02-27 21:13:33 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  
+  var i=0;
+  var a=0;
+  
+
+
+	while(i<str.length)
+	{
+		if(str[i]=='('||str[i]=='['||str[i]=='{')
+		{
+			a++;
+		}
+		if(str[i]==')'||str[i]==']'||str[i]=='}')
+		{
+			a--;
+		}
+		i++;
+	}
+
+	if(a==0)
+	{
+		return true;
+	}
+	else{
+		return false;
+	}
+
+
+
 }
